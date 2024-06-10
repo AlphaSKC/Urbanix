c322e93bb6c6f45cf2cc7ad406a7cc84970141c6
ErnestoJESB <108556821+ErnestoJESB@users.noreply.github.com>
2024-06-09 20:53:42 -0500

    FT: Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import './App.css';
 import Header from './Components/layout/Header';
 import Footer from './Components/layout/Footer';
 import HomeMain from './Components/Home/HomeMain';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './Components/Login/LoginPage';
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <HomeMain></HomeMain>
+      <Routes>
+        <Route path="/" element={<HomeMain/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
       <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
